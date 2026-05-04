c242403734e7a11448b2a2ab6bce41acf2227672
Alkendi-1 <a.alkendi26@gmail.com>
2026-05-04 13:22:12 +0200

    setup fullstack project with MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("StaffSync API is running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
