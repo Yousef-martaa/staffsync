1072682e9ddec915e73a0ae2a2f0ade402e4d02d
Alkendi-1 <a.alkendi26@gmail.com>
2026-05-05 09:20:01 +0200

    feat: create user routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
