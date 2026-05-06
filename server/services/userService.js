073a6798c22bb5623951432af73d42da470c40fa
Alkendi-1 <a.alkendi26@gmail.com>
2026-05-06 02:34:01 +0200

    feat: improve signup security

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,8 +1,20 @@
 import User from "#models/User";
+import bcrypt from "bcrypt";
 
 const createUser = async (userData) => {
-  const user = await User.create(userData);
+  const existingUser = await User.findOne({
+    email: userData.email,
+  });
 
+  if (existingUser) {
+    throw new Error("Email already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(userData.password, 10);
+  const user = await User.create({
+    ...userData,
+    password: hashedPassword,
+  });
   return user;
 };
 
