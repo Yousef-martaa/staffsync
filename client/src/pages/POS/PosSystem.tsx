8172836cc889cea5e43fb479baa69b969019e07c
Morgan Lindbom <din-email@example.com>
2026-05-14 23:31:03 +0200

    feat(client): add routed pages and folders

---
+++ client/src/pages/POS/PosSystem.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function PosSystem() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Back Home</Link>
+      </nav>
+
+      <h1>POS System</h1>
+      <p>Welcome to the Point Of Sale system.</p>
+    </div>
+  );
+}
